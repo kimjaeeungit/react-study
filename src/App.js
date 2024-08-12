30db1dd9fa8d350437b6f4a9637d176dc92675f5
Jaeeun Kim <dmjfhe12@gmail.com>
2024-08-12 16:09:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Box from './component/Box';
 
 function App() {
+  let counter = 0;
+  const [counter2, setCounter2] = useState(0); //[배열,함수]
+  const increase = () => {
+    counter = counter + 1;
+    setCounter2(counter2 + 1);
+    console.log('counter는', counter, 'counter2 state는', counter2);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Box name="리사" num={1} />
+      <Box name="제니" num={2} />
+      <Box name="지수" num={3} /> */}
+
+      <div>{counter}</div>
+      <div>state:{counter2}</div>
+      <button onClick={increase}>클릭</button>
     </div>
   );
 }
